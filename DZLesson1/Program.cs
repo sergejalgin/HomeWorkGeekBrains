fe796ff2245580d456f8f82d2deb60e601d8dde0
Algin <algin86@mail.ru>
2022-12-04 00:15:38 +0300

    Task4

--- DZLesson1/Program.cs
+++ DZLesson1/Program.cs
@@ -1,1 +1,29 @@
-﻿Console.WriteLine("Hello, World!");
+﻿/*Задача 4: Напишите программу, которая принимает 
+на вход три числа и выдаёт максимальное из этих чисел.
+2, 3, 7 -> 7
+44 5 78 -> 78
+22 3 9 -> 22*/
+
+int a, b, c;
+
+int max = 0;
+
+Console.Write("Введите первое число = ");
+int.TryParse(Console.ReadLine()!, out a);
+Console.Write("Введите второе число = ");
+int.TryParse(Console.ReadLine()!, out b);
+Console.Write("Введите третье число = ");
+int.TryParse(Console.ReadLine()!, out c);
+
+if (b > max)
+{
+    max = b;
+}
+if (c > max)
+{
+    max = c;
+    Console.WriteLine($"Максимальное число = {c}");
+}
+else
+    Console.WriteLine($"Максимальное число = {max}");
+
