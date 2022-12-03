15e164a4f38fce7651282ea7f16f42662f590866
Algin <algin86@mail.ru>
2022-12-04 01:59:25 +0300

    Task8

--- DZLesson1/Program.cs
+++ DZLesson1/Program.cs
@@ -1,1 +1,21 @@
-﻿Console.WriteLine("Hello, World!");
+﻿/*Задача 8: Напишите программу, которая на вход принимает число (N), 
+а на выходе показывает все чётные числа от 1 до N.
+
+5 -> 2, 4
+8 -> 2, 4, 6, 8*/
+
+int N;
+
+Console.Write($"Введите число = ");
+int.TryParse(Console.ReadLine()!, out N);
+
+int number = 1;
+
+while (number <= N)
+{
+    if (number % 2 == 0)
+    {
+        Console.WriteLine($"{number}");
+    }
+    number = number + 1;
+}
