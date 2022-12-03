447eecc40b9f6bb71de8a6dd7c6f6a92192f2258
Algin <algin86@mail.ru>
2022-12-04 00:59:12 +0300

    Task6

--- DZLesson1/Program.cs
+++ DZLesson1/Program.cs
@@ -1,1 +1,16 @@
-﻿Console.WriteLine("Hello, World!");
+﻿/*Задача 6: Напишите программу, которая на вход принимает число 
+и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+4 -> да
+-3 -> нет
+7 -> нет*/
+
+
+int N;
+
+Console.Write($"Введите число = ");
+int.TryParse(Console.ReadLine()!, out N);
+
+if (N % 2 == 0)
+    Console.WriteLine($"Да");
+else
+    Console.WriteLine($"Нет");
