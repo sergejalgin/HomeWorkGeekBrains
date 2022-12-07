096a38b1a5ed7030ea69aa2ba282ffa363739872
Algin <algin86@mail.ru>
2022-12-08 01:50:47 +0300

    Task27

--- DZLesson2/Program.cs
+++ DZLesson2/Program.cs
@@ -8,7 +8,11 @@
 int N;
 Console.Write($"Ввведите число = ");
 int.TryParse(Console.ReadLine()!, out N);
+double sum = 0;
+while (N > 0)
+{
+    sum = sum + N % 10;
+    N = N / 10;
 
-
-
-Console.Write($"{sum}");
+}
+Console.WriteLine($"-> {sum}");
