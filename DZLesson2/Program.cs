661d70969480cf4633c682187e0c109280adbdc8
Algin <algin86@mail.ru>
2022-12-07 23:55:03 +0300

    Task25

--- DZLesson2/Program.cs
+++ DZLesson2/Program.cs
@@ -1,23 +1,19 @@
-﻿/*Задача 21
-Напишите программу, которая принимает на вход координаты двух точек 
-и находит расстояние между ними в 3D пространстве.
-A (3,6,8); B (2,1,-7), -> 15.84
-A (7,-5, 0); B (1,-1,9) -> 11.53
+﻿/*Задача 25: Напишите цикл, который принимает на вход два числа (A и B) 
+и возводит число A в натуральную степень B.
+3, 5 -> 243 (3⁵)
+2, 4 -> 16
 */
-int xa, ya, za, xb, yb, zb;
-Console.Write($"Ввведите координаты x точки A = ");
-int.TryParse(Console.ReadLine()!, out xa);
-Console.Write($"Ввведите координаты y точки A = ");
-int.TryParse(Console.ReadLine()!, out ya);
-Console.Write($"Ввведите координаты y точки A = ");
-int.TryParse(Console.ReadLine()!, out za);
-Console.Write($"Ввведите координаты x точки B = ");
-int.TryParse(Console.ReadLine()!, out xb);
-Console.Write($"Ввведите координаты y точки B = ");
-int.TryParse(Console.ReadLine()!, out yb);
-Console.Write($"Ввведите координаты y точки B = ");
-int.TryParse(Console.ReadLine()!, out zb);
 
-double ans = Math.Sqrt((xb - xa) * (xb - xa) + (yb - ya) * (yb - ya) + (zb - za) * (zb - za));
+int a, b;
 
-Console.WriteLine($"Расстояние между точками А и B = {Math.Round(ans, 2)}");
+Console.Write($"Введите число А = ");
+int.TryParse(Console.ReadLine()!, out a);
+Console.Write($"Введите число B = ");
+int.TryParse(Console.ReadLine()!, out b);
+
+int f = a;
+for (int i = 2; i <= b; i++)
+{
+    f = f * a;
+}
+Console.WriteLine($"{f}");
