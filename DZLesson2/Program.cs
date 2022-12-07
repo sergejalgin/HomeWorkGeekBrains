a0f38b3698e59896a6e0c0dc0d0b1b1b8d078c73
Algin <algin86@mail.ru>
2022-12-08 01:30:16 +0300

    Task29,

--- DZLesson2/Program.cs
+++ DZLesson2/Program.cs
@@ -9,9 +9,9 @@ int[] array = { 1, 2, 5, 7, 19, 6, 1, 33 };
 Console.Write($"-> ");
 for (int i = 0; i < array.Length - 3; i++)
 {
-    Console.Write($"{array[i]} ");
+    Console.Write($"{array[i]},");
 }
 Console.WriteLine();
 Console.Write($"-> ");
-Console.Write($"{array[5]} {array[6]} {array[7]}");
+Console.Write($"{array[5]},{array[6]},{array[7]}");
 Console.WriteLine();
