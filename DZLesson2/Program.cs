9412629f6b1955be23f46f1edafe13784d475a37
Algin <algin86@mail.ru>
2022-12-10 00:35:04 +0300

    Task36

--- DZLesson2/Program.cs
+++ DZLesson2/Program.cs
@@ -1,25 +1,24 @@
-﻿/*Задача 34: Задайте массив заполненный случайными положительными 
-трёхзначными числами. Напишите программу, 
-которая покажет количество чётных чисел в массиве.
-[345, 897, 568, 234] -> 2
+﻿/*Задача 36: Задайте одномерный массив, заполненный случайными числами. 
+Найдите сумму элементов, стоящих на нечётных позициях.
+
+[3, 7, 23, 12] -> 19
+[-4, -6, 89, 6] -> 0
 */
 
-int[] array = new int[1];
+int[] array = new int[4];
 
 for (int i = 0; i < array.Length; i++)
-    array[i] = new Random().Next(100, 1000);
+    array[i] = new Random().Next(-100, 100);
 
 PrintArray(array);
+Console.WriteLine();
 
-
-// 
-// {
-//     int even = array[i] % 2 == 0 ? 0 : 1;
-//     Console.Write($"Количество четных чисел = {even}");
-// }
-// Console.Write($"Нет четных чисел"); 
-// }
-
+int sum = 0;
+for (int j = 1; j < array.Length; j += 2)
+{
+    sum = sum + array[j];
+}
+Console.WriteLine($"Сумма = {sum}");
 
 
 void PrintArray(int[] arr)
@@ -27,6 +26,5 @@ void PrintArray(int[] arr)
     for (int i = 0; i < arr.Length; i++)
     {
         Console.Write($"{arr[i]} ");
-        Console.WriteLine();
     }
 }
