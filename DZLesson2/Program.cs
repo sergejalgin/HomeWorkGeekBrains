8b6998f66018ca5fc79b102bc7f8db561f054166
Algin <algin86@mail.ru>
2022-12-09 13:38:22 +0300

    Task34-

--- DZLesson2/Program.cs
+++ DZLesson2/Program.cs
@@ -1,17 +1,32 @@
-﻿/*Задача 29: Напишите программу, которая задаёт массив из 8 элементов 
-и выводит их на экран.
-1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
-6, 1, 33 -> [6, 1, 33]
+﻿/*Задача 34: Задайте массив заполненный случайными положительными 
+трёхзначными числами. Напишите программу, 
+которая покажет количество чётных чисел в массиве.
+[345, 897, 568, 234] -> 2
 */
 
-int[] array = { 1, 2, 5, 7, 19, 6, 1, 33 };
+int[] array = new int[1];
 
-Console.Write($"-> ");
-for (int i = 0; i < array.Length - 3; i++)
+for (int i = 0; i < array.Length; i++)
+    array[i] = new Random().Next(100, 1000);
+
+PrintArray(array);
+
+
+// 
+// {
+//     int even = array[i] % 2 == 0 ? 0 : 1;
+//     Console.Write($"Количество четных чисел = {even}");
+// }
+// Console.Write($"Нет четных чисел"); 
+// }
+
+
+
+void PrintArray(int[] arr)
 {
-    Console.Write($"{array[i]},");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"{arr[i]} ");
+        Console.WriteLine();
+    }
 }
-Console.WriteLine();
-Console.Write($"-> ");
-Console.Write($"{array[5]},{array[6]},{array[7]}");
-Console.WriteLine();
