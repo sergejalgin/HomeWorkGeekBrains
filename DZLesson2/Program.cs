c040ba35da7e9b3976fd83ba3e1ec872107d3e6f
Algin <algin86@mail.ru>
2022-12-11 22:20:55 +0300

    Task38

--- DZLesson2/Program.cs
+++ DZLesson2/Program.cs
@@ -7,10 +7,23 @@
 double[] array = new double[5];
 
 for (int i = 0; i < array.Length; i++)
-    array[i] = new Random().Next(1, 100);
+    array[i] = new Random().Next(1, 100) / 10.0;
 
 PrintArray(array);
 Console.WriteLine();
+
+double max = array[0];
+double min = array[0];
+
+for (int i = 1; i < array.Length; i++)
+{
+    if (array[i] > max)
+        max = array[i];
+    if (array[i] < min)
+        min = array[i];
+}
+Console.WriteLine($"Результат = {max} - {min} = {max - min}");
+
 
 
 
