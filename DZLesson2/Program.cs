950d1142c235e78b102a938b26694c137f9b1e40
Algin <algin86@mail.ru>
2022-12-12 00:45:55 +0300

    Task43

--- DZLesson2/Program.cs
+++ DZLesson2/Program.cs
@@ -1,29 +1,25 @@
-﻿/*Задача 41: Пользователь вводит с клавиатуры M чисел. 
-Посчитайте, сколько чисел больше 0 ввёл пользователь.
-0, 7, 8, -2, -2 -> 2
-1, -7, 567, 89, 223-> 3
+﻿/*Задача 43: Напишите программу, которая найдёт точку пересечения 
+двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+значения b1, k1, b2 и k2 задаются пользователем.
+b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
 */
 
-int M = 1;
+double b1, k1, b2, k2;
 
-List<int> array = new List<int>();
 
-for (int i = 0; i < 5; i++)
-{
-    Console.Write($"Введите число = ");
-    int.TryParse(Console.ReadLine()!, out M);
-    if (M != 0)
-        array.Add(M);
-}
-Console.WriteLine();
-int count = 0;
-for (int i = 0; i < array.Count; i++)
-{
-    if (array[i] > 0)
-        count += 1;
-}
-Console.WriteLine($"Количество чисел больше 0 = {count}");
+Console.Write("Введите b1: ");
+double.TryParse(Console.ReadLine()!, out b1);
+Console.Write("Введите k1: ");
+double.TryParse(Console.ReadLine()!, out k1);
+Console.Write("Введите b2: ");
+double.TryParse(Console.ReadLine()!, out b2);
+Console.Write("Введите k2: ");
+double.TryParse(Console.ReadLine()!, out k2);
 
+double x = (b2 - b1) / (k1 - k2);
+double y = k1 * x + b1;
+
+Console.WriteLine($"Точка пересечения: ({x};{y})");
 
 
 
