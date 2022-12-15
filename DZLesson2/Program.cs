dd80347c771bebcc2e170ecb30bc2df793dca019
Algin <algin86@mail.ru>
2022-12-15 23:27:37 +0300

    Task58

--- DZLesson2/Program.cs
+++ DZLesson2/Program.cs
@@ -8,53 +8,37 @@
 18 20
 15 18
 */
-int[,] array = new int[2, 2]; // Размер 1 массива
-int[,] array2 = new int[3, 3]; // Размер 2 массива
+int[,] array = new int[2, 2] { { 2, 4 }, { 3, 2 } }; // 1 массив
+int[,] array2 = new int[2, 2] { { 3, 4 }, { 3, 3 } };// 2 массив
+int[,] arrayResult = new int[2, 2];
 
-PrintArrayRandom(array); // Генерация чисел 1 массива
-PrintArrayRandom(array2); // Генерация чисел 2 массива
-PrintArray(array); // Вывод рандомного 1 массива
-Console.Write($"|");
-PrintArray(array2); // Вывод рандомного 2 массива
+PrintArray(array); // Вывод 1 массива
+Console.WriteLine();
+PrintArray(array2); // Вывод 2 массива
 Console.WriteLine();
 
-MethodSumArray(array);
-MethodSumArray(array2);
 
 
 
-Console.WriteLine($"{MethodSumArray(array)} + {MethodSumArray(array2)} = {MethodSumArray(array) + MethodSumArray(array2)}");
-
-// Функция суммы элементов массива
-int MethodSumArray(int[,] arr)
+// Результирующая матрица
+if (array.GetLength(0) != array2.GetLength(1))
 {
-    int sum = 0;
-    for (int i = 0; i < arr.GetLength(0); i++)
+    Console.WriteLine(" Выполнить не возможно ");
+    return;
+}
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array2.GetLength(1); j++)
     {
-        for (int j = 0; j < arr.GetLength(1); j++)
+        arrayResult[i, j] = 0;
+        for (int k = 0; k < array.GetLength(1); k++)
         {
-            sum += arr[i, j];
+            arrayResult[i, j] += array[i, k] * array2[k, j];
         }
     }
-    return (sum);
 }
+PrintArray(arrayResult); // Вывод результирующей матрицы
 
-
-
-
-
-//Функция заполнения массива рандомными числами
-void PrintArrayRandom(int[,] arr)
-{
-    for (int i = 0; i < arr.GetLength(0); i++)
-    {
-        for (int j = 0; j < arr.GetLength(0); j++)
-        {
-            array[i, j] = new Random().Next(10);
-        }
-        Console.WriteLine();
-    }
-}
 
 // Функция вывода двумерного массива
 void PrintArray(int[,] arr)
