228211dba6835db91d58de34f20093d2888fac30
Algin <algin86@mail.ru>
2022-12-16 00:44:43 +0300

    Task60

--- DZLesson2/Program.cs
+++ DZLesson2/Program.cs
@@ -1,61 +1,29 @@
-﻿/*Задача 54: Задайте двумерный массив. 
-Напишите программу, которая упорядочит 
-по убыванию элементы каждой строки двумерного массива.
-Например, задан массив:
-1 4 7 2
-5 9 2 3
-8 4 2 4
-В итоге получается вот такой массив:
-7 4 2 1
-9 5 3 2
-8 4 4 2
+﻿/*Задача 60. ...Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. 
+Напишите программу, которая будет построчно выводить массив, 
+добавляя индексы каждого элемента.
+Массив размером 2 x 2 x 2
+66(0,0,0) 25(0,1,0)
+34(1,0,0) 41(1,1,0)
+27(0,0,1) 90(0,1,1)
+26(1,0,1) 55(1,1,1)
 */
-int[,] array = new int[4, 4];
+int[,,] array = new int[2, 2, 2];
 
-PrintArrayRandom(array); // Вывод рандомного массива
-Console.WriteLine();
+PrintArray3(array); // Вывод массива
 
-// Сортировка элементов массива
-for (int i = 0; i < array.GetLength(0); i++)
-{
-    for (int j = 1; j < array.GetLength(1); j++)
-    {
-        for (int k = 0; k < array.GetLength(1) - 1; k++)
-        {
-            if (array[i, k] < array[i, k + 1])
-            {
-                (array[i, k + 1], array[i, k]) = (array[i, k], array[i, k + 1]);
-
-            }
-        }
-    }
-}
-
-PrintArray(array); // Вывод сортированного массива
-
-//Функция вывода массива с рандомными числами
-void PrintArrayRandom(int[,] arr)
+// Функция вывода трёхмерного массива с индексами
+void PrintArray3(int[,,] arr)
 {
     for (int i = 0; i < arr.GetLength(0); i++)
     {
-        for (int j = 0; j < arr.GetLength(0); j++)
+        for (int j = 0; j < arr.GetLength(1); j++)
         {
-            array[i, j] = new Random().Next(10);
-            Console.Write($"{arr[i, j]} ");
+            Console.WriteLine();
+            for (int k = 0; k < arr.GetLength(2); k++)
+            {
+                arr[i, j, k] = new Random().Next(90);
+                Console.Write($"{arr[i, j, k]}({i},{j},{k}) ");
+            }
         }
-        Console.WriteLine();
-    }
-}
-
-// Функция вывода двумерного массива
-void PrintArray(int[,] array)
-{
-    for (int i = 0; i < array.GetLength(0); i++)
-    {
-        for (int j = 0; j < array.GetLength(1); j++)
-        {
-            Console.Write($"{array[i, j]} ");
-        }
-        Console.WriteLine();
     }
 }
