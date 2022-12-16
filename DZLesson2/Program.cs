472b1f92a5fef0351778f062a177e9fbccfe455e
Algin <algin86@mail.ru>
2022-12-16 13:23:51 +0300

    Task64

--- DZLesson2/Program.cs
+++ DZLesson2/Program.cs
@@ -1,1 +1,19 @@
-﻿Console.WriteLine("Hello, World!");
+﻿/*Задача 64: Задайте значение N. Напишите программу, 
+которая выведет все натуральные числа в промежутке от N до 1. 
+Выполнить с помощью рекурсии.
+
+N = 5 -> "5, 4, 3, 2, 1"
+N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"*/
+
+int N;
+Console.Write($"Введите число N = ");
+int.TryParse(Console.ReadLine()!, out N);
+
+Console.WriteLine($"{Number(N, 1)}");
+
+string Number(int left, int right)
+{
+    if (left <= right)
+        return right.ToString();
+    return $"{left} {Number(left - 1, right)}";
+}
